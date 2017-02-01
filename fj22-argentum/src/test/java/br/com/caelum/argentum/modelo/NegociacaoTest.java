4147dde04de5a907f52c00864333b4fefbb2c21f
lab6601 <lab6601@caelum144-06>
2017-02-01 21:03:52 -0200

    Criacao do teste "paraNegociacoesDeTresDiasDistintosGeraTresCandles"

--- fj22-argentum/src/test/java/br/com/caelum/argentum/modelo/NegociacaoTest.java
+++ fj22-argentum/src/test/java/br/com/caelum/argentum/modelo/NegociacaoTest.java
@@ -1,7 +1,9 @@
 package br.com.caelum.argentum.modelo;
 
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -60,6 +62,44 @@ public class NegociacaoTest {
 		
 		Negociacao negociacao = new Negociacao(40.0, 100, segundoAno);
 		Assert.assertFalse(negociacao.isMesmoDia(primeiroAno));
+	}
+	
+	@Test
+	public void paraNegociacoesDeTresDiasDistintosGeraTresCandles(){
+		Calendar hoje = Calendar.getInstance();
+		
+		Negociacao negociacao1 = new Negociacao(40.5, 100, hoje);
+		Negociacao negociacao2 = new Negociacao(40.0, 100, hoje);
+		Negociacao negociacao3 = new Negociacao(39.8, 100, hoje);
+		Negociacao negociacao4 = new Negociacao(42.3, 100, hoje);
+		
+		Calendar amanha = (Calendar) hoje.clone();
+		amanha.add(Calendar.DAY_OF_MONTH, 1);
+		
+		Negociacao negociacao5 = new Negociacao(48.8, 100, amanha);
+		Negociacao negociacao6 = new Negociacao(49.3, 100, amanha);
+		
+		Calendar depois = (Calendar) amanha.clone();
+		depois.add(Calendar.DAY_OF_MONTH, 1);
+		
+		Negociacao negociacao7 = new Negociacao(51.8, 100, depois);
+		Negociacao negociacao8 = new Negociacao(52.3, 100, depois);
+		
+		List<Negociacao> negociacoes = Arrays.asList(negociacao1, negociacao2, 
+				negociacao3, negociacao4, negociacao5, negociacao6, 
+				negociacao7, negociacao8);
+		
+		CandlestickFactory fabrica = new CandlestickFactory();
+		
+		List<Candlestick> candles = fabrica.constroiCandles(negociacoes);
+		
+		Assert.assertEquals(3, candles.size());
+		Assert.assertEquals(40.5, candles.get(0).getAbertura(), 0.00001);
+		Assert.assertEquals(42.3, candles.get(0).getFechamento(), 0.00001);
+		Assert.assertEquals(48.8, candles.get(1).getAbertura(), 0.00001);
+		Assert.assertEquals(49.3, candles.get(1).getFechamento(), 0.00001);
+		Assert.assertEquals(51.8, candles.get(2).getAbertura(), 0.00001);
+		Assert.assertEquals(52.3, candles.get(2).getFechamento(), 0.00001);
 	}
 
 }
